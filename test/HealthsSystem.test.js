ad89582c492aa0ae2a63709cc77a975bd755776e
Guillermo C. Martinez <guillermo.c.martinez@guidesmiths.com>
2018-04-20 22:41:54 +0200

    Added chai

--- test/HealthsSystem.test.js
+++ test/HealthsSystem.test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const expect = require('chai').expect
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const provider = ganache.provider();
@@ -7,22 +8,22 @@ const web3 = new Web3(provider);
 const { interface, bytecode } = require('../ethereum/build/HealthSystem.json');
 
 let accounts;
-let inbox;
+let healthSys;
 
 describe ('Inbox', () => {
   beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
     // Use one of those accounts to deploy the contract
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    healthSys = await new web3.eth.Contract(JSON.parse(interface))
       .deploy({ data: bytecode, arguments: [] })
       .send({ from: accounts[0], gas: '1000000' });
   
-    inbox.setProvider(provider);
+    healthSys.setProvider(provider);
   });
 
   it('deploys a contract', () => {
-    assert.ok(inbox.options.address);
+    expect(healthSys.options.address).to.not.be.undefined;
   });
 
   /*
@@ -30,11 +31,13 @@ describe ('Inbox', () => {
     const message = await inbox.methods.message().call();
     assert.equal(message, 'Hi there!');
   });
-
-  it.skip('it sets a new message', async () => {
-    await inbox.methods.setMessage('new message').send({ from: accounts[0] });
-    const message = await inbox.methods.message().call();
-    assert.equal(message, 'new message');
-  });
   */
+  it.skip('should add a doctor', async () => {
+    // await healthSys.methods.setMessage('new message').send({ from: accounts[0] });
+    await healthSys.methods.addDoctor(accounts[1]).send({ from: accounts[0] });
+    // const message = await inbox.methods.message().call();
+    const doctor = await healthSys.method.getDoctor(accounts[1]).call();
+    expect()
+  });
+  
 });
