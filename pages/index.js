06ca8da439fe928b9261a067b3e3fc151fd6a188
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-04-20 21:00:55 +0200

    first react component for the index

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class HealthySystemIndex extends Component {
+  render() {
+    return <h3>Welcome to the Healthy System!!</h3>;
+  };
+};
+
+export default HealthySystemIndex;
