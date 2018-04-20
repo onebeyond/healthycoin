996f8702645668f0f5ae5c2498f781bdb96ea72a
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-04-20 21:31:43 +0200

    adding a mock header

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+
+export default () => {
+  return (
+    <h4>This is the header</h4>
+  );
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
+import Header from './Header';
 
 export default props => {
   return (
