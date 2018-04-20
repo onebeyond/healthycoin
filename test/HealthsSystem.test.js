83893ff69984b5a7dee87f9d9a66e77dce011956
Josep Servat <josep.servat@guidesmiths.com>
2018-04-20 21:39:18 +0200

    Adding test folder for testing solidity contracts

---
+++ test/HealthsSystem.test.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const { interface, bytecode } = require('../ethereum/contracts');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  // Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  inbox.setProvider(provider);
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+  it('it sets a new message', async () => {
+    await inbox.methods.setMessage('new message').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'new message');
+  });
+});
