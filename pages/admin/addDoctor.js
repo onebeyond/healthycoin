c7de189b6f92b29a570700da7a7709f10b563308
Josep Servat <josep.servat@guidesmiths.com>
2018-04-21 11:51:24 +0200

    adding admin folder structures

---
+++ pages/admin/addDoctor.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Layout from '../components/Layout';
+import Dashboard from '../components/dashboard/Dashboard';
+
+class DashboardPage extends Component {
+  render() {
+    return (
+      <Layout>
+        <addDoctor/>;
+      </Layout>
+    );
+  };
+};
+
+export default DashboardPage;
