ed638015a72d377cf024ef6c8726a9dd1f697a2a
Josep Servat <josep.servat@guidesmiths.com>
2018-04-21 17:04:35 +0200

    Add doctor list account

--- components/admin/add-doctor/AddDoctor.js
+++ components/admin/add-doctor/AddDoctor.js
@@ -1,90 +1,65 @@
 import React from 'react';
-import {
-  Row, FormGroup, FormControl, HelpBlock, ControlLabel, Button
-} from 'react-bootstrap';
-
-import './style.scss';
+import { Grid, Row, Col, Form, FormGroup, FormControl, HelpBlock, ControlLabel, Button} from 'react-bootstrap';
+import web3 from '../../../ethereum/web3';
+import HealthSystem from '../../../ethereum/healthSystem';
 
 export default class AddDoctor extends  React.Component {
 
   constructor(props, context) {
     super(props, context);
-    this.handleChange = this.handleChange.bind(this);
-    this.getValidationState = this.getValidationState.bind(this);
-    this.state = {
-      value: '',
-      loading: false,
-      errorMessage: ''
-    };
-  }
 
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
+        this.handleChange = this.handleChange.bind(this);
+        this.getValidationState = this.getValidationState.bind(this);
 
-  getValidationState() {
-    const length = this.state.value.length;
-    if (length == 42) return 'success';
-    else if (length == 0) return 'warning';
-    else if (length < 42) return 'error';
-    return null;
-  }
+        this.state = {
+            value: ''
+            };
+        }
 
-  onSubmit = async (event) => {
-      event.preventDefault;
-
-      //const campaign = Campaign(this.props.address);
-
-      this.setState({ loading: true, errorMessage: '' })
-
-      try {
+    async componentDidMount(){
         const accounts = await web3.eth.getAccounts();
+        console.log(accounts);
+        let role = await HealthSystem.methods.getRole(accounts[0]).call();
+        console.log(role);
+    }
 
-       /* await campaign.methods.contribute().send({
-          from: accounts[0],
-          value: web3.utils.toWei(this.state.value, 'ether'),
-        });*/
+    getValidationState() {
+            const length = this.state.value.length;
+            if (length == 42) return 'success';
+            else if (length == 0) return 'warning';
+            else if (length < 42) return 'error';
+            return null;
+    }
 
-        //Router.replaceRoute(`/campaigns/${this.props.address}`);
-      } catch (err) {
-        this.setState({ errorMessage: err.message });
-      }
+    handleChange(e) {
+            this.setState({ value: e.target.value });
+    }
 
-      this.setState({ loading: false, value: ''})
-
-  };
-
-
-  render() {
-    return (
-      <div>
-        <Row className="show-grid">
-          <form
-            style={{
-              margin: '50px',
-              background: '#ffffff90',
-              padding: '50px',
-              borderRadius: '15px'
-            }}
-          >
-            <FormGroup
-                controlId="formBasicText"
-                validationState={this.getValidationState()}
-            >
-              <ControlLabel>Manage Doctors</ControlLabel>
-              <FormControl
-              type="text"
-              value={this.state.value}
-              placeholder="Enter address"
-              onChange={this.handleChange}
-              />
-              <FormControl.Feedback />
-              <HelpBlock>Please be sure your address is an ethereum address .</HelpBlock>
-            </FormGroup>
-            <Button bsStyle="info" type="submit">Add Doctor</Button>
-          </form>
-        </Row>
-    </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+            <Row className="show-grid">
+            <form style={{    margin: '50px',
+    background: '#ffffff90',
+    padding: '50px',
+    borderRadius: '15px'}}>
+                <FormGroup
+                    controlId="formBasicText"
+                    validationState={this.getValidationState()}
+                >
+                    <ControlLabel>Manage Doctors</ControlLabel>
+                    <FormControl
+                    type="text"
+                    value={this.state.value}
+                    placeholder="Enter address"
+                    onChange={this.handleChange}
+                    />
+                    <FormControl.Feedback />
+                    <HelpBlock>Please be sure your address is an ethereum address .</HelpBlock>
+                </FormGroup>
+                <Button bsStyle="info" type="submit">Add Doctor</Button>
+                </form>
+            </Row>
+        </div>);
+    }
 }
