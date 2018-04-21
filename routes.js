924dbe2d4ee6e0c67226b8479feb0006ff531262
Ismael Bakkali <ismaelbacha@hotmail.com>
2018-04-21 21:26:59 +0200

    add patient

--- routes.js
+++ routes.js
@@ -5,6 +5,7 @@ routes
   .add('/admin/add-doctor', '/admin/addDoctor')
   .add('/admin/analysis-metrics', '/admin/analysisMetrics')
   .add('/doctor/submit-analysis/', '/admin/submitAnalysis')
+  .add('/doctor/add-patient', '/doctor/addPatient')
   .add('/owner/add-admin', '/owner/addAdmin');
 
 module.exports = routes;
