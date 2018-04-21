0975e784a3918274928b1ef3c7bf4790c56fbf1f
Josep Servat <josep.servat@guidesmiths.com>
2018-04-21 13:00:26 +0200

    new Form

--- components/admin/add-doctor/AddDoctor.js
+++ components/admin/add-doctor/AddDoctor.js
@@ -1,52 +1,53 @@
 import React from 'react';
-import { Grid, Row, Col, Form, FormGroup, FormControl, HelpBlock, ControlLabel } from 'react-bootstrap';
+import { Grid, Row, Col, Form, FormGroup, FormControl, HelpBlock, ControlLabel, Button} from 'react-bootstrap';
 
 export default class AddDoctor extends  React.Component {
 
     constructor(props, context) {
-        super(props, context);
+    super(props, context);
 
         this.handleChange = this.handleChange.bind(this);
         this.getValidationState = this.getValidationState.bind(this);
 
         this.state = {
-          value: ''
-        };
-      }
+            value: ''
+            };
+        }
 
-      getValidationState() {
-        const length = this.state.value.length;
-        if (length > 10) return 'success';
-        else if (length > 5) return 'warning';
-        else if (length > 0) return 'error';
-        return null;
-      }
+    getValidationState() {
+            const length = this.state.value.length;
+            if (length == 42) return 'success';
+            else if (length == 0) return 'warning';
+            else if (length < 42) return 'error';
+            return null;
+    }
 
-      handleChange(e) {
-        this.setState({ value: e.target.value });
-      }
+    handleChange(e) {
+            this.setState({ value: e.target.value });
+    }
 
-   render() {
-      return (
-        <div>
-        <Row className="show-grid">
-        <form>
-              <FormGroup
-                controlId="formBasicText"
-                validationState={this.getValidationState()}
-              >
-                <ControlLabel>Working example with validation</ControlLabel>
-                <FormControl
-                  type="text"
-                  value={this.state.value}
-                  placeholder="Enter text"
-                  onChange={this.handleChange}
-                />
-                <FormControl.Feedback />
-                <HelpBlock>Validation is based on string length.</HelpBlock>
-              </FormGroup>
-            </form>
-        </Row>
-      </div>);
-   }
+    render() {
+        return (
+            <div>
+            <Row className="show-grid">
+            <form>
+                <FormGroup
+                    controlId="formBasicText"
+                    validationState={this.getValidationState()}
+                >
+                    <ControlLabel>Manage Doctors</ControlLabel>
+                    <FormControl
+                    type="text"
+                    value={this.state.value}
+                    placeholder="Enter address"
+                    onChange={this.handleChange}
+                    />
+                    <FormControl.Feedback />
+                    <HelpBlock>Please be sure your address is an ethereum address .</HelpBlock>
+                </FormGroup>
+                <Button bsStyle="info" type="submit">Add Doctor</Button>
+                </form>
+            </Row>
+        </div>);
+    }
 }
