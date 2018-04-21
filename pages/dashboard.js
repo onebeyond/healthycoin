0beef70e69c720c59bdc9bcfc30dbb6e5437b067
tastafur <ruben.romero@guidesmiths.com>
2018-04-21 13:12:03 +0200

    add bootstrap

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import '../styles/bootstrap.scss';
 import Layout from '../components/commons/Layout/index';
 import Dashboard from '../components/dashboard/Dashboard';
 
