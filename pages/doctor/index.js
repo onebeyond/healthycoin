6ffe561876198417c298d8086ae74c5e91685644
ramirobg94 <ramirobg94@gmail.com>
2018-04-21 19:23:59 +0200

    submit btns

--- pages/doctor/index.js
+++ pages/doctor/index.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import '../../styles/bootstrap.scss';
 import Layout from '../../components/commons/Layout';
 
+import Cards from '../../components/commons/Cards';
+
+const options = [
+  {route: 'a', icon: 'fonendo.png', label: 'Add Doctor'},
+  {route: 'b', icon: 'fonendo.png', label: 'Add Doctor'}
+]
+
 export default class DoctorIndex extends Component {
   render() {
     return (
       <Layout>
         <h3>Doctor page!!</h3>
+        <Cards options={options} />
       </Layout>
     );
+
   }
 };
