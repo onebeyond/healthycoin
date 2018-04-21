dd48106b7e5b9fe764ac545c2ac88b6bda612ed6
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-04-21 18:36:13 +0200

    redirecting to admin page

---
+++ pages/admin/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../../styles/bootstrap.scss';
+import Layout from '../../components/commons/Layout';
+
+export default class AdminIndex extends Component {
+  render() {
+    return (
+      <Layout>
+        <h3>Admin page!!</h3>
+      </Layout>
+    );
+  }
+};
