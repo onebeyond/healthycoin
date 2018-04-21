2024cbb3d461e310944e7dbef4c150d16de180ea
tastafur <ruben.romero@guidesmiths.com>
2018-04-21 21:47:35 +0200

    update dashboard

--- components/dashboard/Dashboard.js
+++ components/dashboard/Dashboard.js
@@ -38,6 +38,43 @@ const donut = {
   }
 };
 
+
+const scorePerDay = [
+  {day: 1, value: 76},
+  {day: 2, value: 50},
+  {day: 3, value: 86},
+  {day: 4, value: 57},
+  {day: 5, value: 98},
+  {day: 6, value: 67},
+  {day: 7, value: 57}
+];
+
+const timeToWord = {
+  day: {
+    1: 'M',
+    2: 'T',
+    3: 'W',
+    4: 'T',
+    5: 'F',
+    6: 'S',
+    7: 'S'
+  },
+  month: {
+    1: 'Jan',
+    2: 'Fer',
+    3: 'Mar',
+    4: 'Apr',
+    5: 'May',
+    6: 'Jun',
+    7: 'Jul',
+    8: 'Aug',
+    9: 'Sep',
+    10: 'Oct',
+    11: 'Nov',
+    12: 'Dec'
+  }
+};
+
 const scorePerMonth = [
   {month: 1, value: 76},
   {month: 2, value: 50},
@@ -79,14 +116,38 @@ const scoreGoals = {
   ]
 };
 
-const CategoryPercentage = ({ data }) => (
+const CategoryPercentage = (props) => (
   <VictoryChart theme={VictoryTheme.material} domainPadding={10}>
-    <VictoryAxis/>
-    <VictoryAxis dependentAxis/>
-    <VictoryBar data={data} x="month" y="value"/>
+    <VictoryAxis
+      dependentAxis
+      tickValues={['']}
+      style={{
+        axis: { stroke: 'none' },
+        ticks: { stroke: 'none'},
+        grid: {
+          stroke: (t) => 'transparent'
+        }
+      }}
+    />
+    <VictoryAxis
+      tickFormat={(t) => timeToWord[props.period][t]}
+      style={{
+        axis: { stroke: '#d0d0d0'},
+        ticks: { stroke: 'none'},
+        grid: {
+          stroke: (t) => 'transparent'
+        }
+      }}
+    />
+    <VictoryBar {...props}/>
   </VictoryChart>
 );
 
+/*barRatio={0.8} cornerRadius={5} data={data} x="month" y="value" style={{
+  data: {
+    fill: (d) => d.x === 12 ? '#d0d0d0' : '#13c1ac',
+  }
+}*/
 const Wheel = (props) => (
   <VictoryPie innerRadius={100} {...props}/>
 );
@@ -135,20 +196,49 @@ const Insights = ({ data }) => (
 );
 
 const MonthlyStats = ({ data }) => (
-  <Row className="show-grid">
+  <Row>
     <Col xs={6} md={6}>
-      <Row className="show-grid">
+      <Row>
         <p>465</p>
       </Row>
-      <Row className="show-grid">
-        <CategoryPercentage data={data}/>
+      <Row>
+        <CategoryPercentage period={'month'} data={data} barRatio={0.8} cornerRadius={5} x="month" y="value" style={{
+          data: {
+            fill: (d) => d.x === 12 ? '#0c9bf9' : '#d0d0d0',
+          }
+        }} />
       </Row>
     </Col>
     <Col xs={6} md={6}>
-      <Row className="show-grid">
+      <Row>
         <p>59 this month</p>
       </Row>
-      <Row className="show-grid">
+      <Row>
+        <Doughnut data={donut} height={300} width={300} options={{tooltips: donut.tooltips}}/>
+      </Row>
+    </Col>
+  </Row>
+);
+
+const WeekStats = ({ data }) => (
+  <Row>
+    <Col xs={6} md={6}>
+      <Row>
+        <p>465</p>
+      </Row>
+      <Row>
+        <CategoryPercentage period={'day'} data={data} barRatio={0.8} cornerRadius={5} x={'day'} y={'value'} style={{
+          data: {
+            fill: (d) => d.x === 7 ? '#13c1ac' : '#d0d0d0',
+          }
+        }} />
+      </Row>
+    </Col>
+    <Col xs={6} md={6}>
+      <Row>
+        <p>59 this month</p>
+      </Row>
+      <Row>
         <Doughnut data={donut} height={300} width={300} options={{tooltips: donut.tooltips}}/>
       </Row>
     </Col>
@@ -178,56 +268,56 @@ export default class Dashboard extends Component {
   render () {
     return (
       <Grid>
-        <Row className="show-grid">
+        <Row className={'show-grid'}>
           <Col xs={6} md={9}>
-            <Row className="show-grid">
+            <Row>
               <Col xs={6} md={6}>
-                <MonthlyStats data={scorePerMonth} />
+                <WeekStats data={scorePerDay} />
               </Col>
               <Col xs={6} md={6}>
                 <MonthlyStats data={scorePerMonth} />
               </Col>
             </Row>
-            <Row className="show-grid">
+            <Row>
               <Col xs={8} md={8}>
-                <Row className="show-grid">
+                <Row>
                   <p>Overall</p>
                 </Row>
-                <Row className="show-grid">
+                <Row>
                   <LinesChart data={scorePerMonth} />
                 </Row>
               </Col>
               <Col xs={4} md={4}>
-                <Row className="show-grid">
+                <Row>
                   <p>At a glance 2017</p>
                 </Row>
-                <Row className="show-grid">
+                <Row>
                   <Insights data={scorePerMonth} />
                 </Row>
               </Col>
             </Row>
           </Col>
           <Col xs={6} md={3}>
-            <Row className="show-grid">
+            <Row>
               <Col xs={12} md={12}>
                 <h4>+1.6 ETH</h4>
                 <h5>Rewards last month</h5>
               </Col>
             </Row>
-            <Row className="show-grid">
+            <Row>
               <Col xs={12} md={12}>
                 <SpiderChart data={scoreGoals} />
               </Col>
             </Row>
-            <Row className="show-grid">
-              <Row className="show-grid">
+            <Row>
+              <Row>
                 Monthly goals
               </Row>
-              <Row className="show-grid">
+              <Row>
                 <MonthlyGoals data={scoreGoals}/>
               </Row>
             </Row>
-            <Row className="show-grid">
+            <Row>
               <Col xs={12} md={12}>
               </Col>
             </Row>
