7e0b1926573f69ac403914fc8f390d3934b38591
tastafur <ruben.romero@guidesmiths.com>
2018-04-22 14:01:54 +0200

    change folder doctor to admin

--- pages/admin/submitThresholds.js
+++ pages/admin/submitThresholds.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../../styles/bootstrap.scss';
 
 import Layout from '../../components/commons/Layout/index';
-import SubmitThresholds from '../../components/doctor/submit-thresholds/SubmitThresholds';
+import SubmitThresholds from '../../components/admin/submit-thresholds/SubmitThresholds';
 
 export default class SubmitThresholdsPage extends Component {
     render() {
