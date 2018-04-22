48f08dfeeaf00abcc140edd088a59179d201d746
tastafur <ruben.romero@guidesmiths.com>
2018-04-22 14:41:32 +0200

    update styles

--- components/admin/IndexDash/index.js
+++ components/admin/IndexDash/index.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
+import './style.scss';
+
 import DashCard from '../../commons/DashCard';
 import { Grid, Row, Col } from 'react-bootstrap';
 
 export default class IndexDash extends Component {
   static async getInitialProps() {
     const accounts = await web3.eth.getAccounts();
-    console.log('accounts', accounts)
+    console.log('accounts', accounts);
     return { accounts };
   };
 
   render() {
     return (
-      <Grid>
-        <Row className="show-grid">
+      <Grid className={'wrapper-all-dash'}>
+        <Row>
           <Col md={6}>
-            <DashCard icon="a" label="3" subLabel="Administrators" />
+            <div className={'wrapper-dashcard wrapper-dashcard-odd'}>
+              <DashCard icon="a" label="3" subLabel="Administrators" customStyle={'dashcard-tread'} />
+            </div>
           </Col>
           <Col md={6}>
-            <DashCard icon="a" label="3" subLabel="Doctors" />
+            <div className={'wrapper-dashcard'}>
+              <DashCard icon="a" label="3" subLabel="Doctors" customStyle={'dashcard-tread'} />
+            </div>
           </Col>
         </Row>
         <Row className="show-grid">
           <Col md={6}>
-            <DashCard icon="a" label="3" subLabel="Patients" />
+            <div className={'wrapper-dashcard wrapper-dashcard-odd'}>
+              <DashCard icon="a" label="3" subLabel="Patients" customStyle={'dashcard-tread'} />
+            </div>
           </Col>
           <Col md={6}>
-            <DashCard icon="a" label="3" subLabel="Analysis submitted" />
+            <div className={'wrapper-dashcard'}>
+              <DashCard icon="a" label="3" subLabel="Analysis submitted" customStyle={'dashcard-tread'} />
+            </div>
           </Col>
         </Row>
       </Grid>
--- components/commons/DashCard/index.js
+++ components/commons/DashCard/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 import './style.scss';
 
-const DashCard = ({icon, label, subLabel}) =>
-  <div className="dashcard appear"> 
+const DashCard = ({icon, label, subLabel, customStyle}) =>
+  <div className={'dashcard appear ' + customStyle}>
   <img  src={`/static/${icon}`} alt="" />
     <span className={"dashcard-label"}>{label}</span>
     <span className={"dashcard-subLabel"}>{subLabel}</span>
-</div>
+  </div>;
 
 export default DashCard;
