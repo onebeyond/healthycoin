1000d7912b26cc0fce1fb9e6615944631b157bf8
Josep Servat <josep.servat@guidesmiths.com>
2018-04-22 14:29:47 +0200

    implementing header changes

--- components/commons/Layout/index.js
+++ components/commons/Layout/index.js
@@ -12,29 +12,30 @@ const rolesPath = {
   2: 'doctor',
   3: 'patient',
 };
+
 export default class Layout extends Component {
   static async getInitialProps() {
     const accounts = await web3.eth.getAccounts();
-    console.log('accounts', accounts)
     return { accounts };
   };
 
   state = {
     role: -1,
-    account: ''
+    account: '',
+    roleName: ''
   }
 
   async componentDidMount() {
     const accounts = await web3.eth.getAccounts();
     const myRole = await healthSystem.methods.getMyRole().call({ from: accounts[0] });
-    this.setState({ role: myRole, account: accounts[0] })
+    this.setState({ role: myRole, account: accounts[0], roleName: rolesPath[myRole]})
   }
 
   render() {
     return (
       <div className={'layout'}>
        <img className={'background-top'} src={'/static/background.png'} alt="" />
-        <Header role={'admin'} />
+        <Header role={this.state.roleName} role2={this.state.roleName}/>
         <Grid style={{ minHeight: 'calc(100vh - 200px)' }}>{this.props.children}</Grid>
         <Footer />
       </div>
